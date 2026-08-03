c8d2aa6dcd996924c2b9d5f37f712257ba754f10
mirkobechini <mirkobechini@gmail.com>
2026-08-03 09:27:01 +0200

    fix: rimosso UsersTableSeeder dal DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UsersTableSeeder::class,
             EquipmentTypeTableSeeder::class,
             VehicleTypeTableSeeder::class,
             VehicleTypeEquipmentRequirementSeeder::class,
